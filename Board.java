da1ec99e187e53e40211624339828dd03e607886
houdghiri2021 <77796266+houdghiri2021@users.noreply.github.com>
2022-10-05 22:03:17 -0400

    comments added by Prof. Oudghiri

--- Board.java
+++ Board.java
@@ -27,6 +27,7 @@ public class Board {
         readBoard(filename); // read in provided board
 
         // represent the number of each digit that is left to be placed on the board
+        // this for loop should be executed before you read the board because inside readBoard you will need it
         for (int i = 0; i < 9; i++){
             availableNumbers.add(9);
         }
@@ -39,22 +40,24 @@ public class Board {
             Scanner read = new Scanner(file);
             for (int i = 0; i < 9; i++){
                 for(int j = 0; j < 9; j++){
-                    if(board.get(i).get(j) != EMPTY){
-                        if(isAvailable(read.nextInt())){
-                            board.get(i).set(j, read.nextInt());
-                            availableNumbers.set(read.nextInt()-1, availableNumbers.get(read.nextInt()-1)-1);
-                            checkMove(i, j);
-                        }
-                        else{
-                            System.out.println("Cannot be placed");
-                        }
-                    }
-                    else{
-                        board.get(i).set(j, read.nextInt());
-                    }
+                    //if(board.get(i).get(j) != EMPTY){ // all cells are empty when you start reading from the file
+                        //if(isAvailable(read.nextInt())){
+                            int number = read.nextInt(); // you read the number once for each cell
+                            board.get(i).set(j, number);
+                            availableNumbers.set(number-1, availableNumbers.get(number-1)-1);
+                            //checkMove(i, j); do not call checkMove here
+                        //}
+                        //else{
+                        //    System.out.println("Cannot be placed");
+                        //}
+                    //}
+                    //else{
+                    //    board.get(i).set(j, read.nextInt());
+                    //}
                 }
             }
             read.close();
+            // call checkMove here after you read the complete board - use two nested loops i, i and check each cell (i, j)
         }
         catch(FileNotFoundException e){
             System.out.println("File not found");
@@ -65,25 +68,20 @@ public class Board {
     }
 
     private boolean isAvailable(int digit){
-        for(int i = 0; i < 9; i++){
-            if(digit == availableNumbers.get(i) && availableNumbers.get(i) > 0){
+            if(availableNumbers.get(digit-1) > 0){
                 return true;
             }
-        }
         return false;
     }
 
     private boolean noNumbersLeft(){
         boolean isZero = true;
         for(int i = 0; i < 9; i++){
-            if(availableNumbers.get(i) == 0){
-                isZero = true;
+            if(availableNumbers.get(i) > 0){
+                return false;
             } 
-            else{
-                isZero = false;
-            }
         }
-        return isZero;
+        return true;
     }
 
     private boolean checkMove(int row, int col){
@@ -92,18 +90,18 @@ public class Board {
         int r = row - row % 3;
         int c = col - col % 3;
         for(int i = 0; i < 9; i++){
-            if(board.get(row).get(i) == num){
+            if(board.get(row).get(i) == num){ // check also if i != col
                 b = false;
             }
         }
         for(int i = 0; i < 9; i++){
-            if(board.get(i).get(col) == num){
+            if(board.get(i).get(col) == num){ // check if i!= row too
                 b = false;
             }
         }
         for(int i = r; i < r + 3; i++){
             for(int j = c; j < c + 3; j++){
-                if(board.get(i).get(j) == num){
+                if(board.get(i).get(j) == num){ // check also if i!=row && j!=col
                     b = false;
                 }
             }
